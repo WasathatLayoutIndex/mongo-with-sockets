12b5e669b06f87f110b6b838d4d199a7893e46d4
WasathatLayoutIndex <wasath.theekshana@layoutindex.lk>
2023-08-10 12:26:52 +0530

    feat: realtime update

--- src/seats/seats.gateway.ts
+++ src/seats/seats.gateway.ts
@@ -3,18 +3,29 @@ import {
   SubscribeMessage,
   MessageBody,
   WebSocketServer,
+  OnGatewayConnection,
 } from '@nestjs/websockets';
 import { SeatsService } from './seats.service';
 import { CreateSeatDto } from './dto/create-seat.dto';
 import { UpdateSeatDto } from './dto/update-seat.dto';
 import { Seat } from './schema/seat.schema';
-import { Server } from 'socket.io';
-@WebSocketGateway()
-export class SeatsGateway {
+import { Server, Socket } from 'socket.io';
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+  namespace: '/seats',
+})
+export class SeatsGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
   constructor(private readonly seatsService: SeatsService) {}
+
+  async handleConnection(client: Socket) {
+    const allSeats = await this.seatsService.findAll();
+    client.emit('allSeats', allSeats);
+  }
 
   @SubscribeMessage('createSeat')
   async create(
@@ -23,27 +34,26 @@ export class SeatsGateway {
   ): Promise<Seat> {
     const newSeat = await this.seatsService.create(seat);
 
-    this.server.emit('findAllSeats', await this.seatsService.findAll());
+    this.server.emit('allSeats', await this.seatsService.findAll());
 
     return newSeat;
   }
 
-  @SubscribeMessage('findAllSeats')
+  @SubscribeMessage('allSeats')
   findAll(): Promise<Seat[]> {
     return this.seatsService.findAll();
   }
 
   @SubscribeMessage('updateSeat')
   async update(
-    @MessageBody('seat') 
+    @MessageBody('seat')
     seat: UpdateSeatDto,
-
   ): Promise<Seat> {
-    const { id, ...updatedSeatData } = seat; 
+    const { id, ...updatedSeatData } = seat;
 
     const updatedSeat = await this.seatsService.update(id, updatedSeatData);
-    
-    this.server.emit('findAllSeats', await this.seatsService.findAll());
+
+    this.server.emit('allSeats', await this.seatsService.findAll());
 
     return updatedSeat;
   }
